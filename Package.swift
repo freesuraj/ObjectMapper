2e54dc8c4a49509a4a9fe4dbc3e4c217b37ea6ef
Suraj Pathak <freesuraj@gmail.com>
2020-05-15 12:31:22 +1000

    Make project spm compatible

--- Package.swift
+++ Package.swift
@@ -1,5 +1,20 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-    name: "ObjectMapper"
+    name: "ObjectMapper",
+    platforms: [
+        .iOS(.v11)
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "ObjectMapper",
+            dependencies: []),
+        .testTarget(
+            name: "ObjectMapperTests",
+            dependencies: ["ObjectMapper"]),
+    ]
 )
